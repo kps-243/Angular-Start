27b405f23b5bf4433b804334f7cab338590a32db
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-03-04 21:53:21 +0100

    Fin simplebinding

--- controller/inputvalidation.js
+++ controller/inputvalidation.js
@@ -1,5 +1,5 @@
 myApp = angular.module('inputValidation', []); //Stocke le module dans la variable myApp
-myApp.controller('validControl', ['$scope', function($scope){ //Crée le controller
+myApp.controller('validControl', ['$scope', function($scope){ //Crée le controller qui se nomme validControl
     $scope.submitform = function(isvalid){ // Vérifie que le formulaire est valide
         if(isvalid){ //Si le formulaire est valide alors
             alert('Data was save'); // Alerte que les données sont sauvegardées
