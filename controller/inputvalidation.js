a5f744d76dde2b74cc0db4f3f565b2cce7ba43ad
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-03-04 20:14:57 +0100

    Fonctionnement de inputvalidation

--- controller/inputvalidation.js
+++ controller/inputvalidation.js
@@ -1,5 +1,5 @@
-myApp = angular.module('inputValidation', []);
-myApp.controller('validControl', ['$scope', function($scope){
+myApp = angular.module('inputValidation', []); //Stocke le module dans la variable myApp
+myApp.controller('validControl', ['$scope', function($scope){ //Crée le controller
     $scope.submitform = function(isvalid){
         if(isvalid){
             alert('Data was save');
