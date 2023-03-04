74b8172441fd3789b7fa0cc501210ab07329c7a9
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-03-04 21:26:43 +0100

    Fin valid Déb simpleBinding

--- controller/inputvalidation.js
+++ controller/inputvalidation.js
@@ -1,8 +1,8 @@
 myApp = angular.module('inputValidation', []); //Stocke le module dans la variable myApp
 myApp.controller('validControl', ['$scope', function($scope){ //Crée le controller
-    $scope.submitform = function(isvalid){
-        if(isvalid){
-            alert('Data was save');
+    $scope.submitform = function(isvalid){ // Vérifie que le formulaire est valide
+        if(isvalid){ //Si le formulaire est valide alors
+            alert('Data was save'); // Alerte que les données sont sauvegardées
         }
     };
 }]);
---
+++ controller/simplebinding.js
@@ -0,0 +1,4 @@
+var myApp = angular.module('SimpleBinding',[]);
+myApp.controller('MyControlller', ['$scope', function($scope) {
+$scope.customer = {}
+}]);
