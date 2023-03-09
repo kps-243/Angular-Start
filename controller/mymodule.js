80b4a61def9b2b2982e082048329b2630db82a38
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-03-09 20:05:28 +0100

    Com module

--- controller/mymodule.js
+++ controller/mymodule.js
@@ -1,4 +1,9 @@
+// Déclaration du module "canvas"
 var myApp = angular.module('canvas',[]);
+
+// Définition du contrôleur "sayHello" pour le module "canavas"
 myApp.controller('sayHello', ['$scope', function($scope) {
+
+// Définit une propriété name à la variable globale $scope, qui est un tableau avec un attribut text.
 $scope.name = { text: "Morgan" };
 }]);
