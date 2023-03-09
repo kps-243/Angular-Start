eea4e7936b9f4cae1996592549d5e539e6424cff
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-03-09 14:40:01 +0100

    Com simplefilter

--- controller/simplefilter.js
+++ controller/simplefilter.js
@@ -1,12 +1,18 @@
-var myApp = angular.module('SimpleFilter',[]);
+// Déclaration d'un module AngularJS nommé 'SimpleFilter'
+var myApp = angular.module('SimpleFilter', []);
+
+// Définition d'un contrôleur nommé 'MyControlller' pour le module 'SimpleFilter'
 myApp.controller('MyControlller', ['$scope', function($scope) {
-$scope.customers = [
-{name:'andi',email:'andi@email.com',country:'UK'},
-{name:'brown',email:'brown@email.com',country:'NL'},
-{name:'jake',email:'jake@email.com',country:'DE'},
-{name:'jane',email:'jane@email.com',country:'UK'},
-{name:'mike',email:'mike@email.com',country:'US'},
-{name:'xemmy',email:'xemmy@email.com',country:'US'},
-{name:'zahra',email:'zahra@email.com',country:'CA'}
-]
+
+  // Définition d'un tableau de clients dans la portée du contrôleur
+  $scope.customers = [
+    {name:'andi',email:'andi@email.com',country:'UK'},
+    {name:'brown',email:'brown@email.com',country:'NL'},
+    {name:'jake',email:'jake@email.com',country:'DE'},
+    {name:'jane',email:'jane@email.com',country:'UK'},
+    {name:'mike',email:'mike@email.com',country:'US'},
+    {name:'xemmy',email:'xemmy@email.com',country:'US'},
+    {name:'zahra',email:'zahra@email.com',country:'CA'}
+  ];
+
 }]);
