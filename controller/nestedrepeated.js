895c010d83aa562ad8b3ef38f3ae87f20a6ef95b
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-03-09 19:37:57 +0100

    Com nestedrepeat

--- controller/nestedrepeated.js
+++ controller/nestedrepeated.js
@@ -1,18 +1,34 @@
+// Déclaration d'un module AngularJS nommé 'SimpleFilter'
 var myApp = angular.module('NestedRepeatBinding',[]);
+
+// Définition d'un contrôleur nommé 'MyControlller' pour le module 'SimpleFilter'
 myApp.controller('MyControlller', ['$scope', function($scope) {
+
+// Définit une propriété "orders" sur la super variable globale $scope qui est un objet
 $scope.orders = [
-{
-id:1,
-transid:'123456',
-customer:'user1',
-created:'3/1/2014',
-details: [
-{name:'product1', quantity:2, price:2.5},
-{name:'product2', quantity:1, price:1.4},
-{name:'product3', quantity:3, price:3.5},
-{name:'product4', quantity:5, price:2.8}
-]
-},
+
+    // Ajoute un tableau représentant une commande
+    {
+        // Attribue un identifiant unique à la commande
+        id:1,
+        
+        // Attribue un identifiant de transaction à la commande
+        transid:'123456',
+        
+        // Attribue un nom de client à la commande
+        customer:'user1',
+        
+        // Attribue une date de création à la commande
+        created:'3/1/2014',
+        
+        // Ajoute un tableau d'objets représentant les détails de la commande
+        details: [
+            {name:'product1', quantity:2, price:2.5},
+            {name:'product2', quantity:1, price:1.4},
+            {name:'product3', quantity:3, price:3.5},
+            {name:'product4', quantity:5, price:2.8}
+        ]
+    },
 {
 id:2,
 transid:'123457',
