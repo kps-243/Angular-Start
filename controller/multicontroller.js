455c11a52aa5624ca3ead87ba8fa92b5b883cf64
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-03-09 19:51:51 +0100

    Com multicontroller

--- controller/multicontroller.js
+++ controller/multicontroller.js
@@ -1,14 +1,30 @@
+// Déclaration du module "multiController"
 var myApp = angular.module('multiController', []);
+
+// Définition du contrôleur "Math1" pour le module "multiController"
 myApp.controller('Math1',['$scope', function ($scope){
+   /* Définit une propriété qui joue le rôle de variable, "result" sur la super variable globale $scope,
+    initialisée à la chaîne de caractères '0' */
     $scope.result = '0';
+
+    // Définit une fonction "add" sur la super variable globale $scope, prenant deux arguments "a" et "b"
     $scope.add = function(a,b){
-        $scope.result = a + b;
-    };
+    
+    // Ajoute "a" et "b" et stocke le résultat dans la propriété "result"
+    $scope.result = a + b;
+};
 
 }]);
+
+// Définition du contrôleur "Math1" pour le module "multiController"
 myApp.controller('Math2', ['$scope', function($scope){
     $scope.result = '0';
+
+    /* Définit une fonction "multiple" sur la super variable globale $scope,
+     prenant deux arguments "a" et "b" */
     $scope.multiple = function(a,b){
+
+        //Multiplie a et b et stocke le résultat dans la propriété result.
           $scope.result = a*b;
     }
 }]);
