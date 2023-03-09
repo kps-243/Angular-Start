d04d4e88f7f5c703a1d6b0410b5d946d92a973f8
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-03-09 14:31:09 +0100

    Com logservice

--- controller/logservice.js
+++ controller/logservice.js
@@ -1,5 +1,10 @@
+// Définit une application AngularJS nommée "MyService"
 var myApp = angular.module('LogService',[]);
+
+// Définit un contrôleur pour l'application myApp, en utilisant la méthode controller() d'AngularJS.
 myApp.controller('LogController', function($scope,$log) {
-$scope.$log = $log;
-$scope.message = 'AngularJS';
-});
+    // Attache la valeur de $log à la propriété $log du $scope.
+    $scope.$log = $log;
+    // Initialise la propriété $scope.message avec la chaîne de caractères "AngularJS".
+    $scope.message = 'AngularJS';
+  });
